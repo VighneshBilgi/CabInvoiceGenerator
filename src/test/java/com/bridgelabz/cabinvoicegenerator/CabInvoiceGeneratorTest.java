7fca2a3a4559c1fa4ce513f711d2c42bb7b5faa7
VighneshBilgi <vhbilgi@gmail.com>
2022-06-21 19:56:49 +0530

    Added: UC3_Enhanced_Invoice

--- src/test/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGeneratorTest.java
@@ -2,9 +2,17 @@ package com.bridgelabz.cabinvoicegenerator;
 
 import junit.framework.TestCase;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CabInvoiceGeneratorTest {
+
+
+    CabInvoiceGenerator cabInvoiceGenerator;
+    @Before
+    public void setUp() throws Exception {
+        cabInvoiceGenerator=new CabInvoiceGenerator();
+    }
 
     @Test
     public void givenDistanceAndTime_ShouldReturnFare() {
@@ -24,13 +32,22 @@ public class CabInvoiceGeneratorTest {
         Assert.assertEquals("fare",5,fare,0.0);
     }
 
+//    @Test
+//    public void givenMultipleRides_ShouldReturnTotalFare(){
+//        CabInvoiceGenerator cabInvoiceGenerator=new CabInvoiceGenerator();
+//        Ride[] rides={new Ride(2.0,5),
+//                new Ride(0.1,1)
+//        };
+//        double fare=cabInvoiceGenerator.calculateFare(rides);
+//        Assert.assertEquals("fare",30,fare,0.0);
+//    }
     @Test
-    public void givenMultipleRides_ShouldReturnTotalFare(){
-        CabInvoiceGenerator cabInvoiceGenerator=new CabInvoiceGenerator();
+    public void givenMultipleRides_ShouldReturnInvoiceSummary(){
         Ride[] rides={new Ride(2.0,5),
                 new Ride(0.1,1)
         };
-        double fare=cabInvoiceGenerator.calculateFare(rides);
-        Assert.assertEquals("fare",30,fare,0.0);
+        InvoiceSummary invoiceSummary=cabInvoiceGenerator.calculateFare(rides);
+        InvoiceSummary expected_invoiceSummary=new InvoiceSummary(2,30.0);
+        Assert.assertEquals(expected_invoiceSummary,invoiceSummary);
     }
 }
